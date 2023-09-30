f20a8b3bccc08a2b74a87988b3182895ec3ab12a
Manikanta-Kumar23 <Manikantapadmasolala@gmail.com>
2023-09-30 15:09:11 +0530

    Added Animation using CSS

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,12 +1,14 @@
 import { useMachine } from "@xstate/react"
 import React from "react"
 import { SignalMachine } from "../../machines/SignalMachine/index.tsx"
+import "./index.css"
 
 const Home = () => {
     const [state , send] = useMachine(SignalMachine)
     const redColor = state.matches("play.Red") || state.matches("play.Wait") ? "bg-[#ff0000]" : null
     const yellowColor = state.matches("play.Yellow") ? "bg-[#ffe600]" : null
     const greenColor = state.matches("play.Green") ? "bg-[#6aff00]" : null
+    const flash = state.matches("play.Wait") ? "flashing" : null
     return (
         <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh]">
                 <div className="flex items-center justify-evenly w-[95%]">
@@ -16,7 +18,7 @@ const Home = () => {
                         <div className={`border-[#011c3d] border rounded-[51px] bg-[#45a600] w-[95px] h-[95px] ${greenColor}`}></div>
                     </div>
                     <div className="bg-[#06151c] flex justify-center items-center border-yellow-400 border-[5px] w-[225px] h-[225px]">
-                        {state.matches("play.Red") ? (<span role="img" aria-label="walk" className="text-[75px]">🚶</span>) : (<span role="img" aria-label="wait" className="text-[75px] transition-opacity">✋</span>)}
+                        {state.matches("play.Red") ? (<span role="img" aria-label="walk" className="text-[75px]">🚶</span>) : (<span role="img" aria-label="wait" className={`text-[75px] ${flash}`}>✋</span>)}
                         <h1 className={`ont-[Arial, Helvetica, sans-serif] text-[75px] leading-[26.63px] text-yellow-400 p-[5px] m-[3px] ml-[3%] font-[600] ${state.context.count <1 ? "invisible" : "visible"} `}>
                             {state.context.count}
                         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flashing: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+
+        }
+      } ,
+      animation: {
+        'flash': 'flashing 2s cubic-bezier(0.4, 0, 0.6, 1) infinite'
+      }
+    },
   },
   plugins: [],
 }
